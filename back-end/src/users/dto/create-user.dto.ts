b85b77a8551c00998059d9ab95792ddfd6e30132
AyushDeuja <ayushdeuja11@gmail.com>
2025-04-16 18:58:02 +0545

    user dto created

--- back-end/src/users/dto/create-user.dto.ts
+++ back-end/src/users/dto/create-user.dto.ts
@@ -1,3 +1,23 @@
+import { IsNotEmpty, IsOptional, IsString } from 'class-validator';
+
 export class CreateUserDto {
+  @IsString()
+  @IsNotEmpty()
   name: string;
+
+  @IsString()
+  @IsNotEmpty()
+  email: string;
+
+  @IsString()
+  @IsNotEmpty()
+  mobile: string;
+
+  @IsString()
+  @IsNotEmpty()
+  password: string;
+
+  @IsString()
+  @IsOptional()
+  role_id: string;
 }
