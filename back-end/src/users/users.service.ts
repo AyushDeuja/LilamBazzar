51a403afea46154eacc3538699e1c742b5885920
AyushDeuja <ayushdeuja11@gmail.com>
2025-04-16 20:43:50 +0545

    bcrypt added

--- back-end/src/users/users.service.ts
+++ back-end/src/users/users.service.ts
@@ -7,11 +7,18 @@ import { PrismaClient } from 'generated/prisma';
 export class UsersService {
   constructor(private readonly prisma: PrismaClient) {}
   async create(createUserDto: CreateUserDto) {
+    // Check if the user already exists by email or mobile
     let user = await this.prisma.user.findUnique({
       where: { email: createUserDto.email },
     });
     if (user) {
       throw new BadRequestException('This email is already in use');
+    }
+    user = await this.prisma.user.findUnique({
+      where: { mobile: createUserDto.mobile },
+    });
+    if (user) {
+      throw new BadRequestException('This mobile number is already in use');
     }
   }
 
