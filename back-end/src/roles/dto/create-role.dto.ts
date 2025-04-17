c7d307cb293127643b94979b7a453a92ecd48707
AyushDeuja <ayushdeuja11@gmail.com>
2025-04-17 20:29:19 +0545

    prisma managed

--- back-end/src/roles/dto/create-role.dto.ts
+++
@@ -1,8 +0,0 @@
-import { IsEnum, IsNotEmpty, IsString } from 'class-validator';
-import { roleType } from 'generated/prisma';
-
-export class CreateRoleDto {
-  @IsEnum(roleType)
-  @IsNotEmpty()
-  role_name: string;
-}
