18ba149e061ffc4573f6e96db6565c87535d49b3
AyushDeuja <ayushdeuja11@gmail.com>
2025-04-19 17:51:20 +0545

    app module managed

--- back-end/src/app.module.ts
+++ back-end/src/app.module.ts
@@ -1,10 +1,8 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { RolesModule } from './roles/roles.module';
 
 @Module({
-  imports: [RolesModule],
   controllers: [AppController],
   providers: [AppService],
 })
