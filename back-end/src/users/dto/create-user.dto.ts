b7087a8cb022cafdf15e2bde44a617d5e10251e4
AyushDeuja <ayushdeuja11@gmail.com>
2025-04-19 17:59:12 +0545

    existing user email and password validated

--- back-end/src/users/dto/create-user.dto.ts
+++ back-end/src/users/dto/create-user.dto.ts
@@ -1,3 +1,4 @@
+/* eslint-disable prettier/prettier */
 import { IsEnum, IsNotEmpty, IsOptional, IsString } from 'class-validator';
 import { roleType } from 'generated/prisma';
 
--- back-end/src/users/users.service.ts
+++ back-end/src/users/users.service.ts
@@ -10,17 +10,18 @@ export class UsersService {
   constructor(private readonly prisma: PrismaClient) {}
   async create(createUserDto: CreateUserDto) {
     // Check if the user already exists by email or mobile
-    let user = await this.prisma.user.findUnique({
-      where: { email: createUserDto.email },
+    const existingUser = await this.prisma.user.findFirst({
+      where: {
+        OR: [{ email: createUserDto.email }, { mobile: createUserDto.mobile }],
+      },
     });
-    if (user) {
-      throw new BadRequestException('This email is already in use');
-    }
-    user = await this.prisma.user.findUnique({
-      where: { mobile: createUserDto.mobile },
-    });
-    if (user) {
-      throw new BadRequestException('This mobile number is already in use');
+    if (existingUser) {
+      if (existingUser.email === createUserDto.email) {
+        throw new BadRequestException('This email is already in use');
+      }
+      if (existingUser.mobile === createUserDto.mobile) {
+        throw new BadRequestException('This mobile number is already in use');
+      }
     }
 
     // Hash password here using bcrypt
