ba08c456bb64f33a844778af3772715c5c4bca0e
AyushDeuja <ayushdeuja11@gmail.com>
2025-04-20 17:59:15 +0545

    prisma module added

--- back-end/src/categories/categories.service.ts
+++ back-end/src/categories/categories.service.ts
@@ -1,26 +1,26 @@
 import { Injectable } from '@nestjs/common';
 import { CreateCategoryDto } from './dto/create-category.dto';
 import { UpdateCategoryDto } from './dto/update-category.dto';
+import { PrismaClient } from 'generated/prisma';
 
 @Injectable()
 export class CategoriesService {
-  create(createCategoryDto: CreateCategoryDto) {
-    return 'This action adds a new category';
-  }
+  constructor(private readonly prisma: PrismaClient) {}
+  async create(createCategoryDto: CreateCategoryDto) {}
 
-  findAll() {
+  async findAll() {
     return `This action returns all categories`;
   }
 
-  findOne(id: number) {
+  async findOne(id: number) {
     return `This action returns a #${id} category`;
   }
 
-  update(id: number, updateCategoryDto: UpdateCategoryDto) {
+  async update(id: number, updateCategoryDto: UpdateCategoryDto) {
     return `This action updates a #${id} category`;
   }
 
-  remove(id: number) {
+  async remove(id: number) {
     return `This action removes a #${id} category`;
   }
 }
