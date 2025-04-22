c2fa6d475e820a78e1e2d87f85fbf9e85612de73
AyushDeuja <ayushdeuja11@gmail.com>
2025-04-22 09:43:58 +0545

    products file added

--- back-end/src/app.module.ts
+++ back-end/src/app.module.ts
@@ -4,9 +4,10 @@ import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 import { CategoriesModule } from './categories/categories.module';
 import { CloudinaryModule } from './cloudinary/cloudinary.module';
+import { ProductsModule } from './products/products.module';
 
 @Module({
-  imports: [UsersModule, CategoriesModule, CloudinaryModule],
+  imports: [UsersModule, CategoriesModule, CloudinaryModule, ProductsModule],
   controllers: [AppController],
   providers: [AppService],
 })
