e115d28b4f6d159de2909ce9f77f96582fe950de
AyushDeuja <ayushdeuja11@gmail.com>
2025-04-22 10:19:35 +0545

    product dto created

--- back-end/src/products/dto/create-product.dto.ts
+++ back-end/src/products/dto/create-product.dto.ts
@@ -1,1 +1,10 @@
-export class CreateProductDto {}
+export class CreateProductDto {
+  product_name: string;
+  product_img: string;
+
+  product_price: number;
+  description: string;
+  stock: number;
+  category_id: number;
+  organization_id: number;
+}
