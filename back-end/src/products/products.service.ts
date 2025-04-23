fc9f79b8712fd9bcdc8744411a412bcce68ce8ba
AyushDeuja <ayushdeuja11@gmail.com>
2025-04-23 09:11:48 +0545

    prisma client added in products

--- back-end/src/products/products.service.ts
+++ back-end/src/products/products.service.ts
@@ -1,9 +1,11 @@
 import { Injectable } from '@nestjs/common';
 import { CreateProductDto } from './dto/create-product.dto';
 import { UpdateProductDto } from './dto/update-product.dto';
+import { PrismaClient } from 'generated/prisma';
 
 @Injectable()
 export class ProductsService {
+  constructor(private readonly prisma: PrismaClient) {}
   create(createProductDto: CreateProductDto) {
     return 'This action adds a new product';
   }
