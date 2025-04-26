ce13445df226792ef224149834f2c7e93d85aa78
AyushDeuja <ayushdeuja11@gmail.com>
2025-04-26 19:34:43 +0545

    validation done in bid dto

--- back-end/src/bids/dto/create-bid.dto.ts
+++ back-end/src/bids/dto/create-bid.dto.ts
@@ -1,6 +1,25 @@
+import {
+  IsBoolean,
+  IsDecimal,
+  IsNotEmpty,
+  IsNumber,
+  IsOptional,
+} from 'class-validator';
+
 export class CreateBidDto {
-  product_id: string;
-  bidder_id: string;
+  @IsOptional()
+  @IsNumber()
+  product_id: number;
+
+  @IsOptional()
+  @IsNumber()
+  bidder_id: number;
+
+  @IsNotEmpty()
+  @IsDecimal()
   bid_price: number;
+
+  @IsOptional()
+  @IsBoolean()
   is_winner: boolean;
 }
