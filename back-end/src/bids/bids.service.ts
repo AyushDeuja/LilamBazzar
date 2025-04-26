efa872d5f907b74e66462859d45c4103f2a80162
AyushDeuja <ayushdeuja11@gmail.com>
2025-04-26 20:11:45 +0545

    logic added in update bids

--- back-end/src/bids/bids.service.ts
+++ back-end/src/bids/bids.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable } from '@nestjs/common';
 import { CreateBidDto } from './dto/create-bid.dto';
 import { UpdateBidDto } from './dto/update-bid.dto';
 import { PrismaClient } from 'generated/prisma';
@@ -8,11 +8,11 @@ export class BidsService {
   constructor(private readonly prisma: PrismaClient) {}
   async create(createBidDto: CreateBidDto) {
     //product_id must be unique
-    const existingBid = await this.prisma.bid.findUnique({
+    const existingProduct = await this.prisma.bid.findUnique({
       where: { product_id: createBidDto.product_id },
     });
-    if (existingBid) {
-      throw new Error('Product already exists');
+    if (existingProduct) {
+      throw new BadRequestException('Product already exists');
     }
 
     // bidder_id must be customer not admin or vendor
@@ -20,10 +20,10 @@ export class BidsService {
       where: { id: createBidDto.bidder_id },
     });
     if (!existingCusotmer) {
-      throw new Error('Bidder not found');
+      throw new BadRequestException('Bidder not found');
     }
     if (existingCusotmer.user_role !== 'customer') {
-      throw new Error('Bidder must be a customer');
+      throw new BadRequestException('Bidder must be a customer');
     }
 
     //bidder_id must be unique
@@ -31,12 +31,12 @@ export class BidsService {
       where: { bidder_id: createBidDto.bidder_id },
     });
     if (existingBidder) {
-      throw new Error('Bidder already exists');
+      throw new BadRequestException('Bidder already exists');
     }
 
     //bid_price must be greater than 0
     if (createBidDto.bid_price <= 0) {
-      throw new Error('Bid price must be greater than 0');
+      throw new BadRequestException('Bid price must be greater than 0');
     }
 
     return this.prisma.bid.create({
@@ -55,6 +55,34 @@ export class BidsService {
   }
 
   async update(id: number, updateBidDto: UpdateBidDto) {
+    const existingProduct = await this.prisma.bid.findUnique({
+      where: { product_id: updateBidDto.product_id },
+    });
+    if (existingProduct) {
+      throw new BadRequestException('Product already exists');
+    }
+
+    const existingCusotmer = await this.prisma.user.findUnique({
+      where: { id: updateBidDto.bidder_id },
+    });
+    if (!existingCusotmer) {
+      throw new BadRequestException('Bidder not found');
+    }
+    if (existingCusotmer.user_role !== 'customer') {
+      throw new BadRequestException('Bidder must be a customer');
+    }
+
+    const existingBidder = await this.prisma.bid.findUnique({
+      where: { bidder_id: updateBidDto.bidder_id },
+    });
+    if (existingBidder) {
+      throw new BadRequestException('Bidder already exists');
+    }
+
+    if (updateBidDto.bid_price === undefined || updateBidDto.bid_price <= 0) {
+      throw new BadRequestException('Bid price must be greater than 0');
+    }
+
     return this.prisma.bid.update({
       where: { id },
       data: updateBidDto,
