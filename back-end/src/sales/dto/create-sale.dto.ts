65842ab597cd0ef40b2edd915a15d5eda68d462b
AyushDeuja <ayushdeuja11@gmail.com>
2025-04-28 16:01:58 +0545

    validated the sales dto

--- back-end/src/sales/dto/create-sale.dto.ts
+++ back-end/src/sales/dto/create-sale.dto.ts
@@ -1,6 +1,19 @@
+import { IsDecimal, IsNotEmpty, IsNumber, IsOptional } from 'class-validator';
+
 export class CreateSaleDto {
+  @IsOptional()
+  @IsNumber()
   user_id: number;
+
+  @IsOptional()
+  @IsNumber()
   product_id: number;
+
+  @IsNotEmpty()
+  @IsNumber()
   quantity: number;
+
+  @IsNotEmpty()
+  @IsDecimal()
   total: number;
 }
