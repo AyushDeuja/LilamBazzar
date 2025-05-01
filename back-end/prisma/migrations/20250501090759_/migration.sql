771182b7f99318c5cdf2c765aa9758b43bf00280
AyushDeuja <ayushdeuja11@gmail.com>
2025-05-01 14:57:19 +0545

    schema migration done

---
+++ back-end/prisma/migrations/20250501090759_/migration.sql
@@ -0,0 +1,5 @@
+-- AlterTable
+ALTER TABLE "categories" ALTER COLUMN "category_img" SET DATA TYPE VARCHAR(5000);
+
+-- AlterTable
+ALTER TABLE "product_images" ALTER COLUMN "product_img" SET DATA TYPE VARCHAR(5000);
---
+++ back-end/prisma/migrations/20250501091209_/migration.sql
@@ -0,0 +1,5 @@
+-- AlterTable
+ALTER TABLE "categories" ALTER COLUMN "category_img" SET DATA TYPE TEXT;
+
+-- AlterTable
+ALTER TABLE "product_images" ALTER COLUMN "product_img" SET DATA TYPE TEXT;
